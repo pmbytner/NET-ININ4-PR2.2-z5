5044d2fbf73fc587b05da5e8956d3305553eda7d
Paweł Bytner <pawel.m.bytner@gmail.com>
2022-06-12 13:35:10 +0200

    podstawy wiązania do danych z XML

--- WidokGrupy.xaml.cs
+++ WidokGrupy.xaml.cs
@@ -10,28 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Xml;
 
 namespace NET_ININ4_PR2._2_z5
 {
     /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
+    /// Logika interakcji dla klasy WidokGrupy.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WidokGrupy : Window
     {
-        public MainWindow()
+        public WidokGrupy(System.Xml.XmlNode grupa)
         {
+            DataContext = grupa;
             InitializeComponent();
-        }
-
-        private void Szczegóły(object sender, RoutedEventArgs e)
-        {
-            ListBox lista = (ListBox)this.FindName("ListaGrup");
-            XmlNode grupa = (XmlNode)lista.SelectedItem;
-            if (grupa != null)
-                new WidokGrupy(grupa).Show();
         }
     }
 }
